f7fabd1044a166fe39c537b932aae4e3c8dfc27c
n0s00iu <nitish.sarin@walmartlabs.com>
2018-10-02 17:23:30 +0530

    localstorage used and init function created

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,21 @@
 
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
+    todo: [],
+    completed: []
+  };
+
+renderTodoList();
+
+function renderTodoList() {
+    if (!data.todo.length && !data.completed.length) return;
+  
+    for (var i = 0; i < data.todo.length; i++) {
+      var value = data.todo[i];
+      addItemToDOM(value);
+    }
+  
+    for (var j = 0; j < data.completed.length; j++) {
+      var value = data.completed[j];
+      addItemToDOM(value, true);
+    }
+  }
